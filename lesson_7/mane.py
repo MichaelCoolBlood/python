c32b47c71441dbd0a634a77792b49e54e6481356
movavi_school <abcsmir.ant@gmail.com>
2022-10-30 16:36:12 +0700

    30 октября

--- lesson_7/mane.py
+++ lesson_7/mane.py
@@ -1,26 +1,49 @@
+# try:
+#     number = int(input("введи число:  "))
+#
+#     x = 5 / number
+#     print(x)
+# except ZeroDivisionError:
+#     print("на ноль ты не делиииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииии")
+# except ValueError:
+#     print("хачу цифирки")
+# except Exception:
+#     print("одна ошибка и ты ошився")
+#_____________________________________________________________
+
+
+# try:
+#     name = input("введи имя:  ")
+#     if name == "антон":
+#         raise Exception("это имя под запретом")
+# except Exception as error_m:
+#     print("ненядя", error_m)
+# else:
+#     print("ну вот такое имя можно")
+# finally:
+#     print("Ы")
+#
+# print("смена окончена")
+#_______________________________________________________-
+
+
+# try:
+#     number = int(input("введи число:  "))
+#     x = 5 / number
+# except Exception:
+#     pass
+#
+# if 3 == 3:
+#     pass  # TODO: не забуть написать и купить молока.
 
 
 
+def summa(numbers):
+    return sum(numbers)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(summa([1, 2, 3]))
+assert summa([1, 2]) == 3
+assert summa([1, 2]) == 4
 
 
 
---
+++ lesson_8/lesson__8.py
@@ -0,0 +1,69 @@
+# import random
+# print(random.randint(0, 100))
+
+# import random as r
+# print(r.randint(0, 100))
+
+# from random import randint
+# print(randint(0, 100))
+
+# from random import *
+# print(randint(0, 100))
+
+# import random as r
+# lst = [0, 1, 2, 3, 4, 5]
+# print(r.choice(lst))
+# r.shuffle(lst)
+# print(lst)
+#__________________________________________-
+
+
+import turtle
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+# t.forward(250)
+# t.back(100)
+horizontal = 200
+vertical = 100
+
+t.pensize(5)
+
+t.color("red", "yellow")
+
+t.speed(2)
+
+t.begin_fill()
+
+t.shape("turtle")
+
+t.fd(horizontal)
+t.right(90)
+t.fd(vertical)
+t.rt(90)
+t.fd(horizontal)
+t.rt(90)
+t.fd(vertical)
+t.rt(90)
+t.end_fill()
+
+
+t.penup()
+t.goto(120, -40)
+t.pendown()
+t.fd(50)
+
+t.circle(30)
+
+t.write("momamvi", font =("Arial Black", 25, "normal"), align = "center")
+
+
+
+
+
+
+screen.exitonclick()
+
+
+
+
