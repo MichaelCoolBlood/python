ebfff0234854b5a5f78c5900d0286c11a00bc5b7
movavi_school <pasha.00x.ll.xx.yt@gmail.com>
2023-05-28 16:25:35 +0700

    28 мая, узнал как добыть аксельбант

---
+++ lesson_34/main.py
@@ -0,0 +1,35 @@
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5"}
+# keys = []
+# values = []
+#
+# for igor in slovar:
+#     keys.append(igor)
+#     values.append(slovar[igor])
+# print(keys, values)
+#__________
+slovar = {"key1": "value1",
+          "key2": "value2",
+          "key3": "value3",
+          "key4": "value4",
+          "key5": "value5"}
+print(list(slovar.keys()), list(slovar.values()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
